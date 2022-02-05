56360b55f02790c774b05c9b230b2e29fd8ab766
Fortune Sam-Olayemi <cyb3rcykic@gmail.com>
2022-02-05 13:12:38 +0100

    feat: local message

---
+++ src/disk/disk.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
+
+@Injectable()
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+  getData() {
+    console.log(`Drawing 20 watts from power service`);
+    this.powerService.supplyPower(20);
+    return 'data!';
+  }
+}
